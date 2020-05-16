c0f10e416088cf3904136b81136ec960e3608d6a
Rachid El Habbari <rachid.el.habbari@gmail.com>
2020-05-16 18:12:19 -0400

    Wrapping in the embedded server

--- src/appletv-bootstraper/server.py
+++ src/appletv-bootstraper/server.py
@@ -4,6 +4,8 @@ import ssl
 import json
 import os
 import socket
+import urllib
+import urlparse
 import BaseHTTPServer, SimpleHTTPServer 
 from io import BytesIO
 from airplay import AirPlay
@@ -22,41 +24,58 @@ app_entry_url = 'https://{}/assets/templates/index.xml'
 prod_entry_point = 'chichid-atv2.herokuapp.com'
 application_js = open('application.js', 'r').read(); 
 
+wrap_video_template = """#EXTM3U
+#EXT-X-TARGETDURATION:10
+#EXT-X-VERSION:4
+#EXT-X-MEDIA-SEQUENCE:0
+#EXTINF:10.0,
+{{VIDEO}}
+#EXT-X-ENDLIST"""
+
 ap = AirPlay('127.0.0.1')
 
 class SimpleHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
 	if self.path == "/appletv/js/application.js":
-		app_entry = ''
-		is_prod = socket.gethostbyname(intercepted_app_host) == '127.0.0.1'
+            app_entry = ''
+            is_prod = socket.gethostbyname(intercepted_app_host) == '127.0.0.1'
 
-		if is_prod: 
-			app_entry = app_entry_url.format(prod_entry_point)
-		else:
-			app_entry = app_entry_url.format(intercepted_app_host)
-		
-		self.send_response(200)
-		self.end_headers()
+            if is_prod: 
+                    app_entry = app_entry_url.format(prod_entry_point)
+            else:
+                    app_entry = app_entry_url.format(intercepted_app_host)
+            
+            self.send_response(200)
+            self.end_headers()
 
-		self.wfile.write(application_js.replace('{{config.MainTemplate}}', app_entry))
+            self.wfile.write(application_js.replace('{{config.MainTemplate}}', app_entry))
+        elif "/wrapVideo" in self.path:
+            parsed = urlparse.urlparse(self.path)
+            video_url = urlparse.parse_qs(parsed.query)['url'][0]
+            print('wrapping ' + video_url)
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/vnd.apple.mpegurl')
+            self.end_headers()
+            self.wfile.write(wrap_video_template.replace('{{VIDEO}}', video_url))
 	else:
-		self.send_response(404)
-		self.end_headers()
-		self.wfile.write(b'Resource {} not found'.format(self.path))
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Resource {} not found'.format(self.path))
 
     def do_POST(self):
 	if self.path == "/play":
-		content_length = int(self.headers['Content-Length'])
-		body = json.loads(self.rfile.read(content_length))
-		videoURL = body['videoUrl']
-		print("attempt to play video " + videoURL)
-		ap.play(videoURL)	
-		self.send_response(200)
-		self.end_headers()
-		self.wfile.write('Playing {}'.format(videoURL))
+            content_length = int(self.headers['Content-Length'])
+            body = json.loads(self.rfile.read(content_length))
+            videoURL = urllib.quote(body['videoUrl'])
+            print("attempt to play video " + videoURL)
+            ap.play("https://127.0.0.1/wrapVideo?url=" + videoURL)	
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write('Playing {}'.format(videoURL))
 	else:
-		self.send_response(501)
-		self.wfile.write('Not Supported')
+            self.send_response(501)
+            self.wfile.write('Not Supported')
 
 httpd = BaseHTTPServer.HTTPServer((bind_to_address, server_port), SimpleHTTPRequestHandler)
 

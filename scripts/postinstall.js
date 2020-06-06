5f8ad83f39c9eb0dafeacd6202dd8353e86fb3fa
Rachid El Habbari <rachid.el.habbari@gmail.com>
2020-06-06 14:23:07 -0400

    Automatic creation of ebin

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,4 @@
 const os = require('os');
 const spawn = require('child_process').spawn;
 
-if (os.arch() === 'x32' || os.arch() === 'x64') {
-  console.log(`[postinstall] installing sharp`);
-  const sharpInstall = spawn('npm', ['install', 'sharp', '--no-save']);
-  process.stdin.pipe(sharpInstall.stdin);
-  sharpInstall.stdout.pipe(process.stdout);
-  sharpInstall.stderr.pipe(process.stderr);
-} else {
-  console.warn(`Skipping sharp as it's only supported for x32 and x64`);
-}
+spawn('chmod', ['777', 'bin']);

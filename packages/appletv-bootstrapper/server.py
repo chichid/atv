2f42565a2f5de0ef35c1490ef6c07019fcc139a3
Rachid El Habbari <rachid.el.habbari@gmail.com>
2020-06-20 16:01:30 -0400

    Updated bootstrap entry point

--- packages/appletv-bootstrapper/server.py
+++ packages/appletv-bootstrapper/server.py
@@ -24,7 +24,7 @@ ssl_key_file = 'certificates/{}.key'.format(intercepted_app)
 ssl_certificate_file = 'certificates/{}.pem'.format(intercepted_app)
 
 app_entry_url = 'https://{}/tv-service/templates/index.xml'
-prod_entry_point = 'tv-service.herokuapp.com'
+prod_entry_point = 'atv-service.herokuapp.com'
 application_js = open('application.js', 'r').read(); 
 
 ap = AirPlay('127.0.0.1')

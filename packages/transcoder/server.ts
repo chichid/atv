40f50f90fd0f03ea60606acf65d98817f4006ade
Rachid El Habbari <rachid.el.habbari@gmail.com>
2020-06-21 19:43:50 -0400

    Fixing Freeze

--- packages/transcoder/server.ts
+++ packages/transcoder/server.ts
@@ -58,7 +58,7 @@ const proxyVideo = async (req, res) => {
     while (start < videoInfo.totalDuration) {
       const chunkDuration = Math.min(videoInfo.totalDuration - start, duration);
       playlist.push(`#EXTINF:${chunkDuration},`);
-      playlist.push(`/transcoder/chunk/${encodeURIComponent(url)}/${start}/${chunkDuration}`);
+      playlist.push(`/transcoder/chunk/${encodeURIComponent(url)}/${start}/${0}`);
       start += chunkDuration;
     }
   } else { 

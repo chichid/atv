6e77d6d2a0a8160c446637dcf67873d5d37c3861
Rachid El Habbari <rachid.el.habbari@gmail.com>
2020-07-06 22:46:50 -0400

    Updated Configs

--- packages/tv-service/config.ts
+++ packages/tv-service/config.ts
@@ -3,7 +3,7 @@ import { rcfg, cfg } from 'common/config';
 export const IptvHttpProxy = rcfg('IPTV_HTTP_PROXY', process.env.IPTV_HTTP_PROXY);
 export const Port = cfg('TV_SERVICE_PORT', 8888);
 export const Addr = cfg('TV_SERVICE_ADDR', null);
-export const TranscoderUrl = rcfg('TRANSCODER_URL', '/transcoder');
+export const TranscoderUrl = rcfg('TRANSCODER_URL');
 export const AppleTvRedirectedApp = rcfg('TV_SERVICE_REDIRECTED_APP', 'https://kortv.com');
 export const DefaultPageSize = 25;
 export const ClientSideTemplateTag = '<!-- CLIENT_SIDE_TEMPLATE -->';
